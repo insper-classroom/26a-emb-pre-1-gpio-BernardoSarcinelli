53d0d2956e48acdd60952a4dcff92935d1e4e536
Bernardo Sarcinelli <bernardohns@al.edu.br>
2026-02-20 11:08:52 -0300

    exercicios

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -13,10 +13,10 @@ int main(void) {
     int result = multiplyNumbers(a, b);
 
     int arr[NUM_ELEMENTS];
-    for(int i = 0; i <= NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
-        arr[i-1] = i;
+    for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
+        arr[i] = i;
 
-        printf("%d \n",arr[i-1]);
+        printf("%d \n",arr[i]);
     }
 
     printf("%d \n", result);
